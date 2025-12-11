e70eb59259c7125d41fbb5fcfcbd94b30ae5dcad
Arylkhanvasilev <140228998+Arylkhanvasilev@users.noreply.github.com>
2025-12-11 16:35:09 +0900

    Delete ma.js

--- ma.js
+++
@@ -1,6 +0,0 @@
-var AFRAME = require('aframe');
-var stereoComponent = require('aframe-stereo-component').stereo_component;
-var stereocamComponent = require('aframe-stereo-component').stereocam_component;
-
-AFRAME.registerComponent('stereo', stereoComponent);
-AFRAME.registerComponent('stereocam', stereocamComponent);
