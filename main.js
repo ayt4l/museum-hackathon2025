e8f2b4b9a7645e7bf6c5987fb5ab90925cc035d2
ayt4l <83638220+ayt4l@users.noreply.github.com>
2025-12-12 13:49:00 +0900

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-var stereoComponent = require('/Users/4/Profession IT - 2025/js/index.js').stereo_component;
-var stereocamComponent = require('/Users/4/Profession IT - 2025/js/index.js').stereocam_component;
-
-AFRAME.registerComponent('stereo', stereoComponent);
-AFRAME.registerComponent('stereocam', stereocamComponent);
